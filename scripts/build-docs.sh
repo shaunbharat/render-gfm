13afa83f84c81e9e30ab3d01a1d318a0c82ccd44
Shaun Bharat <79479981+shaunbharat@users.noreply.github.com>
2024-01-26 14:58:13 -0500

    Rename README.html to index.html in build-docs.sh

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,4 +1,5 @@
 npm run render-gfm -- themes --output-dir ./docs/css/
 npm run render-gfm -- markdown --output-dir ./docs/ README.md
+mv ./docs/README.html ./docs/index.html
 mkdir --parents ./docs/pages/
 cp ./README.md ./docs/pages/README.md
